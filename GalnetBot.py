f79be425c156be7c2c431b62f8719eff141e39fb
DerSchubi <schubi@erlangen.ccc.de>
2022-12-07 15:40:02 +0100

    Add bot functions

--- GalnetBot.py
+++ GalnetBot.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python -u
 # GalnetBot.py
+import disnake
 import json
 import os
 import time
 import urllib.request
 
+from disnake.ext import commands
 from dotenv import load_dotenv
 
-last_time = 0
-
 load_dotenv()
+ENV_TOKEN = os.getenv('DISCORD_TOKEN')
+ENV_GUILD = os.getenv('DISCORD_GUILD')
+ENV_CHANNEL = os.getenv('DISCORD_CHANNEL')
+ENV_STATUS = os.getenv('DISCORD_STATUS')
 ENV_GALNET_URL = os.getenv('DISCORD_GALNET_URL')
 
-def update():
+bot = commands.Bot(
+  command_prefix='/',
+  intents=disnake.Intents.all(),
+  help_command=None,
+  #sync_commands_debug=True,
+)
+
+async def update():
   with urllib.request.urlopen(ENV_GALNET_URL) as url:
     json_load = json.load(url)
 
   for data in reversed(json_load['data']):
-    id = data['id']
+    guild = disnake.utils.get(bot.guilds, name=ENV_GUILD)
+    channel = disnake.utils.get(guild.channels, name=ENV_CHANNEL)
 
+    id = data['id']
     with open('./known_articles.log') as file:
       if id not in file.read():
         article = data['attributes']
@@ -33,32 +46,47 @@ def update():
           print('Galnet News from ' + date)
           print(title + '\n')
 
+          post = 'GelNet News vom ' + date + '\n__**' + title + '**__\n\n'+body
+
           pos = 0
           while pos >= 0:
-            if len(body) > 2000:
-              pos = body.rfind('\r\n', 0, 2000)
+            if len(post) > 2000:
+              pos = post.rfind('\r\n', 0, 2000)
               if pos == -1:
-                pos = body.rfind(' ', 0, 2000)
+                pos = post.rfind(' ', 0, 2000)
                 skip = 1
               else:
                 skip = 2
-              chunk = body[0:0 + pos]
-              body = body[pos + 1 + skip:]
+              chunk = post[0:0 + pos]
+              post = post[pos + 1 + skip:]
             else:
               pos = -1
-              chunk = body
+              chunk = post
 
             print(chunk)
             print('\n')
+            await channel.send(f'{chunk}')
 
           with open('./known_articles.log', 'a') as file:
             file.write(id + '\n')
 
+@bot.event
+async def on_ready():
+  guild = disnake.utils.get(bot.guilds, name=ENV_GUILD)
+  print(
+    f'{bot.user} is connected to the following Guild:\n'
+    f'{guild.name}(id: {guild.id})\n'
+  )
+  await bot.change_presence(activity=disnake.Game(name=ENV_STATUS))
+  update()
+  last_time = 0
+  while True:
+    if time.time() >= last_time + 60:
+      print('Updating...')
+      await update()
+      last_time = time.time()
+      print('done')
 
-while True:
-  if time.time() >= last_time + 60:
-    print('Updating...')
-    update()
-    last_time = time.time()
-    print('done')
-  time.sleep(1)
+bot.run(ENV_TOKEN)
+
+
